7e76a404902a5a28e591516aec9e1f04cebd5d4d
RebelWasTaken <43701247+RebelWasTaken@users.noreply.github.com>
2018-09-29 19:18:42 +0530

    Add files via upload

---
+++ frc-bot-run.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+from discord import Game
+
+
+Client = discord.client
+client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name='Freshiance'))
+    print('Ready, Freddy') 
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('/flip'):
+        randomlist = ["heads","tails",]
+        await client.send_message(message.channel,(random.choice(randomlist)))
+    if message.content == '/logo':
+        em = discord.Embed(description='This logo looks more fresh than ever!')
+        em.set_image(url='https://media.discordapp.net/attachments/495197188834459650/495578707604537397/FRC_NEW_LOGO.png?width=473&height=473')
+        await client.send_message(message.channel, embed=em)
+    if message.content == '/logo':
+        em = discord.Embed(description='This logo looks more fresh than ever!')
+        em.set_image(url='https://media.discordapp.net/attachments/495197188834459650/495578707604537397/FRC_NEW_LOGO.png?width=473&height=473')
+        await client.send_message(message.channel, embed=em)
+    if message.content == '/info':
+        await client.send_message(message.channel,'Freshiance is a Critical Ops Organisation! We specialise in hosting tournaments!')
+client.run('NDkxODYwNzU1MjgxMzQ2NTYy.DpEHcw.WBNWmT48y8XbvxtIcRoQNK_FU2c')
